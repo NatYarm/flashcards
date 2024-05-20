4881070bae507c166f2cdad47d0a89c2d62bc62c
NatYarm <yarmnat7@gmail.com>
2024-05-20 23:19:04 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+// module.exports = {
+  // root: true,
+  // env: { browser: true, es2020: true },
+  // extends: [
+  //   '@it-incubator/eslint-config'
+    // 'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // 'plugin:react-hooks/recommended',
+  //],
+  // ignorePatterns: ['dist', '.eslintrc.cjs'],
+  // parser: '@typescript-eslint/parser',
+  // plugins: ['react-refresh'],
+  // rules: {
+  //   'react-refresh/only-export-components': [
+  //     'warn',
+  //     { allowConstantExport: true },
+  //   ],
+  //},
+
+  
+//}
+
+module.exports = {
+  extends: '@it-incubator/eslint-config',
+  rules: {
+    'perfectionist/sort-imports': 'off'
+  }
+}
