2c44d8f7a9fe51859d3842951589b04decddef0d
Serzh-R <daarksen2@gmail.com>
2024-05-29 17:58:40 +0500

    addad svg.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Link } from 'react-router-dom'
-
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Typography } from '@/components/ui/typography'
 
 export function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Button as={Link} to={'https://google.com'}>
-          Hello
-        </Button>
-
-        <Typography variant={'h1'}>H1</Typography>
-      </BrowserRouter>
+      <Button>Hello</Button>
+      <br />
+      <p></p>
+      <p></p>
+      <Checkbox />
+      <p></p>
+      <p></p>
+      <br />
+      <Typography variant={'h1'}>H1</Typography>
     </div>
   )
 }
