94cf7318d7d06ec3f0696e0ca909d5d37a9dc7ce
Serzh-R <daarksen2@gmail.com>
2024-06-03 00:54:58 +0500

    change card

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,3 +1,5 @@
+import React, { ComponentPropsWithoutRef, ElementType, ReactNode, forwardRef } from 'react'
+
 import { clsx } from 'clsx'
 
 import s from './card.module.scss'
@@ -5,15 +7,18 @@ import s from './card.module.scss'
 export type CardProps<T extends ElementType = 'div'> = {
   as?: T
   children: ReactNode
+  className?: string
+} & ComponentPropsWithoutRef<T>
 
-export const Card = forwardRef(
-    const { as: Component = 'div', children, className, ...restProps } = props
-    const classNames = clsx(s.root, className)
+const Card = forwardRef<ElementType, CardProps<ElementType>>((props, ref) => {
+  const { as: Component = 'div', children, className, ...restProps } = props
+  const classNames = clsx(s.root, className)
 
-    return (
-      <Component className={classNames} {...restProps} ref={ref}>
-        {children}
-      </Component>
-    )
-  }
-)
+  return (
+    <Component className={classNames} {...restProps} ref={ref}>
+      {children}
+    </Component>
+  )
+})
+
+export default Card
