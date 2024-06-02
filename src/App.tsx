224a420c77a061fdad5f7181c6ecd64dac7d9af2
Serzh-R <daarksen2@gmail.com>
2024-06-03 04:21:25 +0500

    added forwardRef to Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Link } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
 import { Select } from '@/components/ui/select'
 import { Typography } from '@/components/ui/typography'
+import { Card } from '@/components/ui/card/card'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label/label'
+import { Pagination } from '@/components/ui/pagination'
 
 export function App() {
   return (
@@ -11,6 +15,14 @@ export function App() {
         <Button as={Link} to={'https://google.com'}>
           Hello
         </Button>
+        <br />
+        <Card />
+        <br />
+        <Checkbox />
+        <br />
+        <Label />
+        <br />
+        <Pagination />
         <br />
         <Select />
         <br />
