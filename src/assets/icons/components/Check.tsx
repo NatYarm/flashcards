bd621700aed041ffd80263f359adf5cd2be20bca
Serzh-R <daarksen2@gmail.com>
2024-06-02 14:49:05 +0500

    change button 2

--- src/assets/icons/components/Check.tsx
+++ src/assets/icons/components/Check.tsx
@@ -2,28 +2,29 @@ import type { SVGProps } from 'react'
 import { Ref, forwardRef, memo } from 'react'
 const SvgCheck = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    fill="none"
-    viewBox="0 0 24 24"
+    fill={'none'}
+    height={'1em'}
     ref={ref}
+    viewBox={'0 0 24 24'}
+    width={'1em'}
+    xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
     <rect
-      width={22}
+      fill={'#fff'}
       height={22}
-      x={1}
-      y={1}
-      fill="#fff"
-      stroke="currentcolor"
-      strokeWidth={2}
       rx={4}
       ry={4}
+      stroke={'currentcolor'}
+      strokeWidth={2}
+      width={22}
+      x={1}
+      y={1}
     />
-    <path stroke="currentcolor" strokeWidth={2} d="m6 12 4 4 8-8" />
+    <path d={'m6 12 4 4 8-8'} stroke={'currentcolor'} strokeWidth={2} />
   </svg>
 )
 const ForwardRef = forwardRef(SvgCheck)
 const Memo = memo(ForwardRef)
+
 export default Memo
