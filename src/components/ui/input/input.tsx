55f9e3c172778754b92321f8688834f97bb596aa
Serzh-R <daarksen2@gmail.com>
2024-06-07 20:06:59 +0500

    change input

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,8 +1,20 @@
-import { ComponentPropsWithoutRef, ElementType, LegacyRef, ReactNode, forwardRef } from 'react'
+import {
+  ChangeEvent,
+  ComponentProps,
+  ComponentPropsWithoutRef,
+  ElementType,
+  LegacyRef,
+  ReactNode,
+  forwardRef,
+  useState,
+} from 'react'
 
+import { EyeOffOutline, EyeOutline } from '@/assets/icons/components'
 import { clsx } from 'clsx'
 
 import s from './input.module.scss'
+
+import { Label } from '../label/label'
 
 export type InputProps<T extends ElementType = 'input'> = {
   as?: T
@@ -11,6 +23,7 @@ export type InputProps<T extends ElementType = 'input'> = {
   error?: boolean
   fullWidth?: boolean
   label?: string
+  onValueChange?: (value: string) => void
   variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
@@ -19,6 +32,8 @@ export const Input = forwardRef(
     props: InputProps<T>,
     ref: LegacyRef<HTMLInputElement> | undefined
   ) => {
+    const [revealPassword, setRevealPassword] = useState(false)
+
     const {
       as: Component = 'input',
       children,
@@ -26,26 +41,61 @@ export const Input = forwardRef(
       error,
       fullWidth,
       label,
+      onChange,
+      onValueChange,
       type,
       variant = 'primary',
       ...restProps
     } = props
+
+    const isRevealPasswordButtonShown = type === 'password'
 
     const classNames = clsx(
       s.input,
       s[variant],
       error && s.error,
       fullWidth && s.fullWidth,
+      isRevealPasswordButtonShown && s.input__showpass,
       className
     )
 
+    function getFinalType(type: ComponentProps<'input'>['type'], showPassword: boolean) {
+      if (type === 'password' && showPassword) {
+        return 'text'
+      }
+
+      return type
+    }
+    const finalType = getFinalType(type, revealPassword)
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange?.(e)
+      onValueChange?.(e.target.value)
+    }
+
+    const handleShowPasswordClick = () => {
+      setRevealPassword((prevState: boolean) => !prevState)
+    }
+
     return (
       <div className={s.inputWrapper}>
-        {label && <label className={s.label}>{label} </label>}
-
-        <Component className={classNames} ref={ref} {...restProps}>
-          {children}
-        </Component>
+        <Label className={s.label}>
+          {label}
+          <Component
+            className={classNames}
+            onChange={handleChange}
+            ref={ref}
+            type={finalType}
+            {...restProps}
+          >
+            {children}
+          </Component>
+          {isRevealPasswordButtonShown && (
+            <button className={s.showPassword} onClick={handleShowPasswordClick} type={'button'}>
+              {revealPassword ? <EyeOffOutline /> : <EyeOutline />}
+            </button>
+          )}
+        </Label>
 
         {error && <span style={{ color: 'red' }}>Error!</span>}
       </div>
