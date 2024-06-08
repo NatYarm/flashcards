40a47e0773b39923f2b27735552cfdd9a01eeab8
Mger-arzi <mger54598@gmail.com>
2024-06-08 10:15:47 +0300

    add ref to dropDown

--- src/components/ui/dropdownMenu/ExampleDropDown.tsx
+++ src/components/ui/dropdownMenu/ExampleDropDown.tsx
@@ -1,3 +1,5 @@
+import { ComponentPropsWithoutRef, ElementType, ForwardedRef, forwardRef } from 'react'
+
 import { LogOut, PersonOutline } from '@/assets/icons/components'
 
 import s from './DropdownMenu.module.scss'
@@ -7,41 +9,49 @@ import { DropDownItem } from './DropDownItem'
 import { DropDownSeparator } from './DropDownSeparator'
 import { DropdownMenuComponent } from './DropdownMenuComponent'
 
-type Props = {
+type Props<T extends ElementType = 'div'> = {
+  as?: T
   email?: string
   name?: string
   photo?: string
   photoDesc?: string
   profilePageHref?: string
-}
-export const ExampleDropDown = ({ email, name, photo, photoDesc, profilePageHref }: Props) => {
-  return (
-    <DropdownMenuComponent trigger={<img alt={photoDesc} className={s.trigger_img} src={photo} />}>
-      <DropDownItem>
-        <div className={s.DropdownMenuItem}>
-          <img alt={photoDesc} className={s.DropdownMenuItem_img} src={photo} />
-          <div className={s.nameAndemail}>
-            <Typography as={'div'} className={s.userName} variant={'subtitle2'}>
-              {name}
-            </Typography>
-            <Typography as={'div'} className={s.userEmail} variant={'caption'}>
-              {email}
-            </Typography>
+} & ComponentPropsWithoutRef<T>
+
+export const ExampleDropDown = forwardRef(
+  <T extends ElementType = 'div'>(props: Props<T>, ref: ForwardedRef<T>) => {
+    const { email, name, photo, photoDesc, profilePageHref } = props
+
+    return (
+      <DropdownMenuComponent
+        trigger={<img alt={photoDesc} className={s.trigger_img} src={photo} />}
+      >
+        <DropDownItem>
+          <div className={s.DropdownMenuItem}>
+            <img alt={photoDesc} className={s.DropdownMenuItem_img} src={photo} />
+            <div className={s.nameAndemail}>
+              <Typography as={'div'} className={s.userName} variant={'subtitle2'}>
+                {name}
+              </Typography>
+              <Typography as={'div'} className={s.userEmail} variant={'caption'}>
+                {email}
+              </Typography>
+            </div>
           </div>
-        </div>
-      </DropDownItem>
-      <DropDownSeparator />
-      <DropDownItem asChild>
-        <a href={profilePageHref}>
-          <PersonOutline className={s.rightSlot} />
-          My Profile
-        </a>
-      </DropDownItem>
-      <DropDownSeparator />
-      <DropDownItem>
-        <LogOut className={s.rightSlot} />
-        Sing out
-      </DropDownItem>
-    </DropdownMenuComponent>
-  )
-}
+        </DropDownItem>
+        <DropDownSeparator />
+        <DropDownItem asChild>
+          <a href={profilePageHref}>
+            <PersonOutline className={s.rightSlot} />
+            My Profile
+          </a>
+        </DropDownItem>
+        <DropDownSeparator />
+        <DropDownItem>
+          <LogOut className={s.rightSlot} />
+          Sing out
+        </DropDownItem>
+      </DropdownMenuComponent>
+    )
+  }
+)
