7466db148856d63f7f3f9193d67f94d14f827f59
Serzh-R <daarksen2@gmail.com>
2024-06-09 22:06:52 +0500

    Change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
 
-import { useState } from 'react'
-import { BrowserRouter, Link } from 'react-router-dom'
-
-import { LoginForm } from '@/components/auth/login-form'
-import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Pagination } from '@/components/ui/pagination'
-
-import {
-  LogOut,
-  MoreHorizontal,
-  MoreVerticalOutline,
-  PersonOutline,
-} from './assets/icons/components'
-import { DropdownMenu, DropdownSeparator } from './components/ui/dropdown'
-import { DropdownMenuItem } from './components/ui/dropdown/dropdownMenuItem'
-import { Tabs } from './components/ui/tabs'
+import { SignIn } from '@/components/auth/sign-in'
 
 export function App() {
   return (
     <div style={{ margin: 'auto', maxWidth: '1000px' }}>
       <BrowserRouter>
-
-
-        <Button as={Link} to={'https://google.com'}>
-          Button
-        </Button>
-        <br />
-        <br />
-        <Card>Card</Card>
-        <br />
-        <Input type={'password'} />
-        <br />
-        <Pagination
-          currentPage={currentPage}
-          itemsPerPage={itemsPerPage}
-          onPageChange={handlePageChange}
-          onPerPageChange={handlePerPageChange}
-          perPageOptions={perPageOptions}
-          totalPageCount={10}
-        />
-        <br />
-        <LoginForm />
+        <SignIn />
       </BrowserRouter>
-
-      <Tabs
-        defaultValue={'tab1'}
-        label={'title'}
-        orientation={'vertical'}
-        tabs={[
-          { title: 'Switcher', value: 'tab1' },
-          { title: 'Switcher', value: 'tab2' },
-          { disabled: true, title: 'Switcher', value: 'tab3' },
-        ]}
-      />
-      <br />
-      <DropdownMenu trigger={<MoreVerticalOutline />}>
-        <DropdownMenuItem>
-          <PersonOutline /> Profile
-        </DropdownMenuItem>
-        <DropdownSeparator />
-        <DropdownMenuItem>
-          <LogOut /> Logout
-        </DropdownMenuItem>
-        <DropdownSeparator />
-      </DropdownMenu>
-
     </div>
   )
 }
