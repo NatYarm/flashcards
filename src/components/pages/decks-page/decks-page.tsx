b191409eddbc2715c8a83af19c398886dee76127
NatYarm <yarmnat7@gmail.com>
2024-06-10 17:37:18 +0300

    create decks page

--- src/components/pages/decks-page/decks-page.tsx
+++ src/components/pages/decks-page/decks-page.tsx
@@ -1,9 +1,27 @@
+import { TrashOutline } from '@/assets/icons/components'
+
 import s from './decks-page.module.scss'
 
-import { Page } from '../../page/page'
 import { Button } from '../../ui/button'
+import { Page } from '../../ui/page/page'
+import { Slider } from '../../ui/slider'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+} from '../../ui/table/table'
+import { Tabs } from '../../ui/tabs/tabs'
 import { TextField } from '../../ui/text-field'
 import { Typography } from '../../ui/typography'
+
+const tabs = [
+  { title: 'My decks', value: 'my' },
+  { title: 'All decks', value: 'all' },
+  { title: 'Favorites', value: 'favorites' },
+]
 
 export const DecksPage = () => {
   return (
@@ -12,12 +30,39 @@ export const DecksPage = () => {
         <Typography as={'h1'} variant={'h1'}>
           Decks List
         </Typography>
-        <Button>Add new deck</Button>
+        <Button>Add New Deck</Button>
       </div>
-      <div>
-        <TextField placeholder={'Search'} type={'search'} />
-        {/* <Tabs></Tabs> */}
+      <div className={s.filters}>
+        <div className={s.searchField}>
+          <TextField placeholder={'Search'} type={'search'} />
+        </div>
+
+        <Tabs label={'Show decks cards'} tabs={tabs} />
+
+        <Slider label={'Number of cards'} value={[2, 10]} />
+        <Button variant={'secondary'}>
+          <TrashOutline />
+          Clear Filter
+        </Button>
       </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Cards</TableHeadCell>
+            <TableHeadCell>Last updated</TableHeadCell>
+            <TableHeadCell>Created by</TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>New Deck</TableCell>
+            <TableCell>4</TableCell>
+            <TableCell>23.12.2023</TableCell>
+            <TableCell>John Dow</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </Page>
   )
 }
--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -4,12 +4,15 @@ import * as SliderPrimitive from '@radix-ui/react-slider'
 import { clsx } from 'clsx'
 
 import s from './slider.module.scss'
+
+import { Label } from '../label/label'
 const Slider = forwardRef<
   ElementRef<typeof SliderPrimitive.Root>,
   {
+    label?: string
     value: (null | number)[]
   } & Omit<ComponentPropsWithoutRef<typeof SliderPrimitive.Root>, 'value'>
->(({ className, max, onValueChange, value, ...props }, ref) => {
+>(({ className, label, max, onValueChange, value, ...props }, ref) => {
   useEffect(() => {
     if (value?.[1] === undefined || value?.[1] === null) {
       onValueChange?.([value?.[0] ?? 0, max ?? 0])
@@ -17,23 +20,26 @@ const Slider = forwardRef<
   }, [max, value, onValueChange])
 
   return (
-    <div className={s.container}>
-      <span className={s.valueDisplay}>{value?.[0]}</span>
-      <SliderPrimitive.Root
-        className={clsx(s.root, className)}
-        max={max}
-        onValueChange={onValueChange}
-        ref={ref}
-        {...props}
-        value={[value?.[0] ?? 0, value?.[1] ?? max ?? 0]}
-      >
-        <SliderPrimitive.Track className={s.track}>
-          <SliderPrimitive.Range className={s.range} />
-        </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className={s.thumb} />
-        <SliderPrimitive.Thumb className={s.thumb} />
-      </SliderPrimitive.Root>
-      <span className={s.valueDisplay}>{value?.[1]}</span>
+    <div>
+      {label && <Label>{label}</Label>}
+      <div className={s.container}>
+        <span className={s.valueDisplay}>{value?.[0]}</span>
+        <SliderPrimitive.Root
+          className={clsx(s.root, className)}
+          max={max}
+          onValueChange={onValueChange}
+          ref={ref}
+          {...props}
+          value={[value?.[0] ?? 0, value?.[1] ?? max ?? 0]}
+        >
+          <SliderPrimitive.Track className={s.track}>
+            <SliderPrimitive.Range className={s.range} />
+          </SliderPrimitive.Track>
+          <SliderPrimitive.Thumb className={s.thumb} />
+          <SliderPrimitive.Thumb className={s.thumb} />
+        </SliderPrimitive.Root>
+        <span className={s.valueDisplay}>{value?.[1]}</span>
+      </div>
     </div>
   )
 })
