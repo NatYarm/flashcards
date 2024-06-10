227e2cf7bd5a6e16a7975fe42bbe04b21abe357c
Serzh-R <daarksen2@gmail.com>
2024-06-10 22:41:43 +0500

    create recovery-password

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
+import { RecoveryPassword } from '@/components/auth/recovery-password'
 import { SignIn } from '@/components/auth/sign-in'
 import { SignUp } from '@/components/auth/sign-up'
 
 export function App() {
   return (
-    <div style={{ margin: 'auto', maxWidth: '1000px' }}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Navigate to={'/sign-in'} />} path={'/'} />
-          <Route element={<SignIn />} path={'/sign-in'} />
-          <Route element={<SignUp />} path={'/sign-up'} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Navigate to={'/sign-in'} />} path={'/'} />
+        <Route element={<SignIn />} path={'/sign-in'} />
+        <Route element={<SignUp />} path={'/sign-up'} />
+        <Route element={<RecoveryPassword />} path={'/recovery-password'} />
+      </Routes>
+    </BrowserRouter>
   )
 }
