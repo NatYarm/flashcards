02b615bffbdace921f1c63e976e2535e28bffead
Serzh-R <daarksen2@gmail.com>
2024-06-12 18:46:44 +0500

    change router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -6,8 +6,6 @@ import {
   createBrowserRouter,
 } from 'react-router-dom'
 
-//import { DeckPage } from '@/pages/deck-page/deck-page'
-
 import { Layout } from './components/layout/layout'
 import { DecksPage } from './components/pages/decks-page/decks-page'
 import { SignInPage } from './components/pages/sign-in-page/sign-in-page'
@@ -29,10 +27,6 @@ const privateRoutes: RouteObject[] = [
     element: <DecksPage />,
     path: '/',
   },
-  // {
-  //   element: <DeckPage />,
-  //   path: '/decks/:deckId',
-  // },
 ]
 
 const router = createBrowserRouter([
