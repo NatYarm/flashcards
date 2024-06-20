e30a325d9b85af22c0609c5fdd33e318d5b632d5
serzh-r <o--s@mail.ru>
2024-06-20 14:20:05 +0500

    create authApi

---
+++ src/features/auth/api/authApi.ts
@@ -0,0 +1,123 @@
+import { toast } from 'react-toastify'
+
+import { baseApi } from '@/services/baseApi'
+import {
+  ForgotPasswordArgs,
+  MeResponse,
+  ResetPasswordArgs,
+  SignInArgs,
+  SignInResponse,
+  SignUpArgs,
+  SignUpResponse,
+} from '@/common/types'
+
+// import { ProfileFormData } from '@/features/auth/profile/ui/PersonalInformation'
+
+export const authApi = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      authToken: builder.mutation<undefined, void>({
+        query: () => ({
+          method: 'POST',
+          url: 'v2/auth/refresh-token',
+        }),
+      }),
+
+      forgotPassword: builder.mutation<undefined, ForgotPasswordArgs>({
+        query: body => ({
+          body,
+          method: 'POST',
+          url: 'v1/auth/recover-password',
+        }),
+      }),
+
+      getMe: builder.query<MeResponse, void>({
+        providesTags: ['Me'],
+        query: () => 'v1/auth/me',
+      }),
+
+      logOut: builder.mutation<undefined, void>({
+        invalidatesTags: ['Me'],
+        async onQueryStarted() {
+          localStorage.removeItem('accessToken')
+          localStorage.removeItem('refreshToken')
+          toast.success('You are logout successfully.', { autoClose: 1000 })
+        },
+        query: () => ({
+          method: 'POST',
+          url: 'v1/auth/logout',
+        }),
+      }),
+
+      resetPassword: builder.mutation<undefined, ResetPasswordArgs>({
+        query: ({ password, token }) => ({
+          body: { password },
+          method: 'POST',
+          url: `v1/auth/reset-password/${token}`,
+        }),
+      }),
+
+      signIn: builder.mutation<SignInResponse, SignInArgs>({
+        invalidatesTags: ['Me'],
+        async onQueryStarted(_, { queryFulfilled }) {
+          const { data } = await queryFulfilled
+
+          if (!data) {
+            return
+          }
+
+          localStorage.setItem('accessToken', data.accessToken)
+          localStorage.setItem('refreshToken', data.refreshToken)
+          toast.success('You are login successfully.', { autoClose: 1000 })
+        },
+        query: body => ({
+          body: body,
+          method: 'POST',
+          url: 'v1/auth/login',
+        }),
+      }),
+
+      signUp: builder.mutation<SignUpResponse, SignUpArgs>({
+        query: body => ({
+          body: body,
+          method: 'POST',
+          url: 'v1/auth/sign-up',
+        }),
+      }),
+
+      // updateMe: builder.mutation<MeResponse, ProfileFormData>({
+			updateMe: builder.mutation<MeResponse, any>({
+        invalidatesTags: ['Me'],
+        query: ({ avatar, name }) => {
+          const formData = new FormData()
+
+          if (name) {
+            formData.append('name', name)
+          }
+          if (avatar) {
+            formData.append('avatar', avatar)
+          } else if (avatar === null) {
+            formData.append('avatar', '')
+          }
+
+          return {
+            body: formData,
+            method: 'PATCH',
+            url: 'v1/auth/me',
+          }
+        },
+      }),
+    }
+  },
+})
+
+export const {
+  useAuthTokenMutation,
+  useForgotPasswordMutation,
+  useGetMeQuery,
+  useLogOutMutation,
+  useResetPasswordMutation,
+  useSignInMutation,
+  useSignUpMutation,
+  useUpdateMeMutation,
+} = authApi
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { baseApi } from './baseApi'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
@@ -8,3 +9,5 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
+
+setupListeners(store.dispatch)
