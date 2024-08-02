d37de12316ff33f342b74b65b268ac7b3b2890ec
Serzh-R <daarksen2@gmail.com>
2024-08-02 22:31:12 +0500

    cange modal.module.scss

--- src/common/components/modal/Modal.tsx
+++ src/common/components/modal/Modal.tsx
@@ -1,3 +1,48 @@
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
+
+import { Close } from '@/assets/icons/components'
+import { Typography } from '@/common/components'
+import * as RadixDialog from '@radix-ui/react-dialog'
+
+import s from './modal.module.scss'
+
+export type ModalProps = {
+  children: ReactNode
+  onCancel?: () => void
+  onConfirm?: () => void
+  onOpenChange: (open: boolean) => void
+  open?: boolean
+  title?: string
+} & Omit<ComponentPropsWithoutRef<typeof RadixDialog.Dialog>, 'onOpenChange' | 'open'>
+
+export const Modal = forwardRef<ElementRef<typeof RadixDialog.Content>, ModalProps>(
+  ({ children, title, ...rest }, ref) => {
+    return (
+      <RadixDialog.Root {...rest}>
+        <RadixDialog.Portal>
+          <RadixDialog.Overlay className={s.overlay} />
+          <RadixDialog.Content className={s.content} ref={ref}>
+            <div className={s.header}>
+              <RadixDialog.Title>
+                <Typography as={'h2'} variant={'h2'}>
+                  {title}
+                </Typography>
+              </RadixDialog.Title>
+              <RadixDialog.Close asChild className={s.closeButton}>
+                <Close className={s.closeIcon} />
+              </RadixDialog.Close>
+            </div>
+            {children}
+          </RadixDialog.Content>
+        </RadixDialog.Portal>
+      </RadixDialog.Root>
+    )
+  }
+)
+
+Modal.displayName = 'Modal'
+
+/*
 import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 import { Close } from '@/assets/icons/components'
@@ -32,3 +77,4 @@ export const Modal = ({ children, title, ...rest }: ModalProps) => {
     </RadixDialog.Root>
   )
 }
+*/
